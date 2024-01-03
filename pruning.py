77572cc26f06efe8e5336d865349ac02283529c6
krasickiPawel <252749@student.pwr.edu.pl>
2024-01-03 23:27:38 +0100

    pruning idea for future

--- pruning.py
+++ pruning.py
@@ -15,48 +15,5 @@ def prune_random(model, prune_amount=0.3):
 def get_conv_layers(model):
     return [module for module in filter(lambda m: type(m) == torch.nn.Conv2d, model.modules())]
 
-#
-# class RandomPruningMethod(prune.BasePruningMethod):
-#     """Prune every other entry in a tensor"""
-#
-#     PRUNING_TYPE = "unstructured"
-#
-#     def compute_mask(self, t, default_mask):
-#         mask = default_mask.clone()
-#         mask.view(-1)[::2] = 0
-#         return mask
 
-
-# parameters_to_prune = [
-#     (module, "weight") for module in filter(lambda m: type(m) == torch.nn.Conv2d, model.modules())
-# ]prune.global_unstructured(
-#     parameters_to_prune,
-#     pruning_method=prune.L1Unstructured,
-#     amount=0.2,
-# )
-# def foobar_unstructured(module, name):
-#     """Prunes tensor corresponding to parameter called `name` in `module`
-#     by removing every other entry in the tensors.
-#     Modifies module in place (and also return the modified module)
-#     by:
-#     1) adding a named buffer called `name+'_mask'` corresponding to the
-#     binary mask applied to the parameter `name` by the pruning method.
-#     The parameter `name` is replaced by its pruned version, while the
-#     original (unpruned) parameter is stored in a new parameter named
-#     `name+'_orig'`.
-#
-#     Args:
-#         module (nn.Module): module containing the tensor to prune
-#         name (string): parameter name within `module` on which pruning
-#                 will act.
-#
-#     Returns:
-#         module (nn.Module): modified (i.e. pruned) version of the input
-#             module
-#
-#     Examples:
-#         >>> m = nn.Linear(3, 4)
-#         >>> foobar_unstructured(m, name='bias')
-#     """
-#     RandomPruningMethod.apply(module, name)
-#     return module
+# custom pruning in the future
